519fab482ef9fc2dff2eca6ae30e8a65b6a0c9f9
Olafur Bogason <olafur@genkiinstruments.com>
2019-02-24 17:03:41 +0000

    updating raw docs sun feb 24 17:03:41 GMT 2019

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying docs to GitHub...\033[0m"
+git checkout docs
+git add .
+
+# Commit changes.
+msg="updating raw docs `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin docs
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+git checkout master
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
